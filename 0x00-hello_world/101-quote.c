e264b016b0210ab60f25e20be53f6665eadf105a
luckizedcoder <luckizeprivate@gmail.com>
2023-12-18 14:42:16 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - A C program that prints a line to the standard error
+ * main - Entry point, prints a string to the standard error
  * Return: Always 1 (Error)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

a4c8dc11310f5dfc6e99067418a8966eabff111c
luckizedcoder <luckizeprivate@gmail.com>
2023-12-18 13:52:49 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,27 +6,27 @@
  */
 int main(void)
 {
-    int ones, tens, hundreds;
+	int hundreds, tens, ones;
 
-    for (hundreds = '0'; hundreds <= '9'; hundreds++)
-    {
-        for (tens = hundreds + 1; tens <= '9'; tens++)
-        {
-            for (ones = tens + 1; ones <= '9'; ones++)
-            {
-                putchar(hundreds);
-                putchar(tens);
-                putchar(ones);
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = hundreds + 1; tens <= '9'; tens++)
+		{
+			for (ones = tens + 1; ones <= '9'; ones++)
+			{
+				putchar(hundreds);
+				putchar(tens);
+				putchar(ones);
 
-                if (!(hundreds == '7' && tens == '8' && ones == '9'))
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
+				if (!(hundreds == '7' && tens == '8' && ones == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }

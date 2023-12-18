efd74d70e8822ffe41ee6562cc10f011a5c1b5fd
luckizedcoder <luckizeprivate@gmail.com>
2023-12-18 12:21:31 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Prog Entry point
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-{
+	{
 	int a;
 
 	for (a = 0; a < 10; a++)

aec622b9fbc4e4687893b9860c7e76eb1f06581b
luckizedcoder <luckizeprivate@gmail.com>
2023-12-18 11:04:19 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Program Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c;
+
+    c = 'a';
+    while (c <= 'z')
+    {
+        if ((c != 'q' && c != 'c') && c <= 'z')
+            putchar(c);
+        c++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}

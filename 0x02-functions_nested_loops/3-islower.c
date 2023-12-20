28726863524ed5aded2ab7247c6514faa7346e5e
luckizedcoder <luckizeprivate@gmail.com>
2023-12-20 08:00:28 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,15 +5,11 @@
  * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
-
 int _islower(int c)
-
 {
-
-	if (c >= 97 && c <= 122)
-	{
-
-	return (1);
-	}
-	return (0);
-}	
+    if (c >= 97 && c <= 122)
+    {
+        return (1);
+    }
+    return (0);
+}
